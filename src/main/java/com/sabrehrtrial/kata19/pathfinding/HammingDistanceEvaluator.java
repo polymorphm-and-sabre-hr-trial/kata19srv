6c65471539e8e19f0e5183929a10b3208b2ff8e3
Andrei Antonov <polymorphm@gmail.com>
2018-07-12 16:57:05 +0300

    pathfinding.HammingDistanceEvaluator: now it passes tests

--- src/main/java/com/sabrehrtrial/kata19/pathfinding/HammingDistanceEvaluator.java
+++ src/main/java/com/sabrehrtrial/kata19/pathfinding/HammingDistanceEvaluator.java
@@ -1,5 +1,6 @@
 package com.sabrehrtrial.kata19.pathfinding;
 
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -11,12 +12,36 @@ public class HammingDistanceEvaluator
     
     @Override
     public boolean evaluateDistance(String word1, String word2) {
-        throw new UnsupportedOperationException();
+        if (word1.length() != word2.length()) {
+            // different words length makes "Hamming Distance" infinite
+            
+            return false;
+        }
+        
+        char[] chr1 = word1.toCharArray();
+        char[] chr2 = word2.toCharArray();
+        int len = chr1.length;
+        
+        int dist = 0;
+        
+        for (int i = 0; i < len; ++i) {
+            if (chr1[i] != chr2[i]) {
+                ++dist;
+                
+                if (dist >= 2) {
+                    // the distance begins from 2 is not appropriate
+                    
+                    return false;
+                }
+            }
+        }
+        
+        return true;
     }
     
     @Override
     public List<Integer> calcBucket(String word) {
-        throw new UnsupportedOperationException();
+        return Collections.singletonList(word.length());
     }
     
 }
