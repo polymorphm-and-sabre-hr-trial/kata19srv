44f8176592d4e032774b9f21e08693b1904df454
Andrei Antonov <polymorphm@gmail.com>
2018-07-12 18:06:59 +0300

    pathfinding.WordNodeIndex: completed! it passes all its tests now

--- src/main/java/com/sabrehrtrial/kata19/pathfinding/WordNodeIndex.java
+++ src/main/java/com/sabrehrtrial/kata19/pathfinding/WordNodeIndex.java
@@ -1,6 +1,9 @@
 package com.sabrehrtrial.kata19.pathfinding;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * a structure for keeping all word's nodes with access to anyone by index.
@@ -28,6 +31,62 @@ public class WordNodeIndex {
     }
     
     /**
+     * add a word and its node to the index.
+     * 
+     * @param word a word for adding to the index.
+     * @param node a word's node for adding to the index.
+     */
+    private void addWordWithNode(String word, WordNode node) {
+        if (allNodeMap.containsKey(word)) {
+            return;
+        }
+        
+        List<Integer> buckets = distEval.calcBucket(word);
+        
+        // we will look for only in these maps, instead of doing brute-force
+        List<HashMap<String, WordNode>> lookingNodeMaps;
+        
+        if (buckets != null) {
+            lookingNodeMaps = new ArrayList<>();
+            
+            buckets.forEach((bucket) -> {
+                lookingNodeMaps.add(nodeMapBuckets.get(bucket));
+            });
+        } else {
+            // in this case we have to look the entire node map
+            
+            lookingNodeMaps = Collections.singletonList(allNodeMap);
+        }
+        
+        ArrayList<WordNode> toConnectList = new ArrayList<>();
+        
+        lookingNodeMaps.forEach((lookingNodeMap) -> {
+            lookingNodeMap.forEach((oWord, oNode) -> {
+                boolean isGood = distEval.evaluateDistance(word, oWord);
+                
+                if (isGood) {
+                    toConnectList.add(oNode);
+                }
+            });
+        });
+        
+        toConnectList.forEach((oNode) -> {
+            WordNode.connectNodes(node, oNode);
+        });
+        
+        // all connections have been made,
+        // only left to add the node to the index store
+        
+        allNodeMap.put(word, node);
+        
+        lookingNodeMaps.stream().filter((lookingNodeMap)
+                -> lookingNodeMap != allNodeMap
+        ).forEach((lookingNodeMap) -> {
+            lookingNodeMap.put(word, node);
+        });
+    }
+    
+    /**
      * add a word's node to the index.
      * 
      * @param node a word's node for adding to the index.
@@ -37,12 +96,9 @@ public class WordNodeIndex {
             throw new NullPointerException("node is null");
         }
         
+        String word = node.getWord();
         
-        
-        // XXX    it is WRONG (incomplete) implementation
-        allNodeMap.put(node.getWord(), node);
-        
-        
+        addWordWithNode(word, node);
     }
     
     /**
@@ -53,7 +109,7 @@ public class WordNodeIndex {
     public void addWord(String word) {
         WordNode node = new WordNode(word);
         
-        addWordNode(node);
+        addWordWithNode(word, node);
     }
     
     /**
