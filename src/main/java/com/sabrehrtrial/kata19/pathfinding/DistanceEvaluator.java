83386f69635ee359df818b84f1a63afb0435d9c9
Andrei Antonov <polymorphm@gmail.com>
2018-07-12 15:54:07 +0300

    pathfinding.DistanceEvaluator: a default function for calculating bucket identifier values.

--- src/main/java/com/sabrehrtrial/kata19/pathfinding/DistanceEvaluator.java
+++ src/main/java/com/sabrehrtrial/kata19/pathfinding/DistanceEvaluator.java
@@ -1,5 +1,7 @@
 package com.sabrehrtrial.kata19.pathfinding;
 
+import java.util.List;
+
 /**
  * reference to function for evaluating a distance.
  */
@@ -13,5 +15,17 @@ public interface DistanceEvaluator {
      * @return true if distance is appropriative. otherwise false.
      */
     boolean evaluateDistance(String word1, String word2);
+    
+    /**
+     * calculate bucket identifier values. these values can be used to prevent
+     * brute-force operation over whole index during evaluating a distance.
+     * 
+     * @param word calculation is over this word.
+     * @return one or a few bucket identifiers. null can be returned if
+     * an interface implementation is not able to calculate buckets.
+     */
+    default List<Integer> calcBucket(String word) {
+        return null;
+    }
     
 }
