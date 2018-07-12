2401427c83f4933f3ed4789ebdff78f0926ac37c
Andrei Antonov <polymorphm@gmail.com>
2018-07-13 00:04:08 +0300

    pathfinding.HammingDistanceEvaluator: huge optimization! now ``calcBucket()`` works via calculation true hash

--- src/main/java/com/sabrehrtrial/kata19/pathfinding/HammingDistanceEvaluator.java
+++ src/main/java/com/sabrehrtrial/kata19/pathfinding/HammingDistanceEvaluator.java
@@ -1,6 +1,6 @@
 package com.sabrehrtrial.kata19.pathfinding;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -9,6 +9,11 @@ import java.util.List;
  */
 public class HammingDistanceEvaluator
         implements DistanceEvaluator {
+    
+    private final int BUCKET_HASH_ITERATIONS = 3;
+    private final int BUCKET_HASH_INIT_VALUE = 1069;
+    private final int BUCKET_HASH_MULTIPLIER = 37;
+    private final int BUCKET_HASH_MODULE = 1000000;
     
     @Override
     public boolean evaluateDistance(String word1, String word2) {
@@ -39,9 +44,44 @@ public class HammingDistanceEvaluator
         return true;
     }
     
+    /**
+     * update bucket hash value via simple formula.
+     * 
+     * @param hash previous value of hash.
+     * @param ch character number value.
+     * @return next value of hash.
+     */
+    private int updateBucketHash(int hash, int ch) {
+        hash += ch;
+        hash *= BUCKET_HASH_MULTIPLIER;
+        hash %= BUCKET_HASH_MODULE;
+        
+        return hash;
+    }
+    
     @Override
     public List<Integer> calcBucket(String word) {
-        return Collections.singletonList(word.length());
+        // we will make two character sequences,
+        // and then calculate hash of each one
+        
+        int seq1Hash = BUCKET_HASH_INIT_VALUE;
+        int seq2Hash = BUCKET_HASH_INIT_VALUE;
+        
+        char[] chs = word.toCharArray();
+        
+        for (int t = 0; t < BUCKET_HASH_ITERATIONS; ++t) {
+            for (int i = 0; i < chs.length; ++i) {
+                int ch = chs[i];
+                
+                if (i % 2 == 0) {
+                    seq1Hash = updateBucketHash(seq1Hash, ch);
+                } else {
+                    seq2Hash = updateBucketHash(seq2Hash, ch);
+                }
+            }
+        }
+        
+        return Arrays.asList(seq1Hash, seq2Hash);
     }
     
 }
